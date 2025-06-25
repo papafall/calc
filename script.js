c1b18a3b1b5ab9b4ecaa4c5b8af35cd64274b368
Papa Fall <papa.fall1@gmail.com>
2025-06-25 11:20:28 -0700

    Initial commit: Add basic calculator layout, styles, and core math functions

---
+++ script.js
@@ -0,0 +1,38 @@
+// Basic math functions
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    return "Error: Division by zero!";
+  }
+  return a / b;
+}
+
+// Operate function
+function operate(operator, a, b) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    default:
+      return null;
+  }
+}
+
+// For testing in the console
+// console.log(operate('+', 3, 5));
